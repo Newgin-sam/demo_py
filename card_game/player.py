a4c36559f3727f82e088652647336abb8a3399e1
Newgin sam <newginsam@gmail.com>
2022-05-03 21:14:43 +0530

    changes

--- card_game/player.py
+++ card_game/player.py
@@ -7,15 +7,14 @@ class Player:
     def remove_one(self):
         return self.all_cards.pop(0)
 
-    def add_one(self,new_cards):
+    def add_one(self, new_cards):
 
         if(type(new_cards) == type([])):
-            #for multiple
+            # for multiple combing the array
             self.all_cards.extend(new_cards)
         else:
-            #for single
+            # for single adding a element
             self.all_cards.append(new_cards)
-
 
     def __str__(self) -> str:
         return f"Player {self.name} has {len(self.all_cards)} cards. "
